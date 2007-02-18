8902eafa28f067384ad0006a0a6dfb44823f7700
pablius <pablius@867dbc27-7028-0410-ac17-f545b9fe687e>
2007-02-18 23:26:00 +0000

    Added behavior to loaded collada model.

--- trunk/Xith3DTest/src/org/test/xith3d/BasicShapesTest.java
+++ trunk/Xith3DTest/src/org/test/xith3d/BasicShapesTest.java
@@ -7,15 +7,23 @@ import org.xith3d.render.base.Xith3DEnvironment;
 import org.xith3d.render.canvas.Canvas3DWrapper;
 import org.xith3d.render.loop.RenderLoop;
 import org.xith3d.scenegraph.*;
+import org.xith3d.spatial.bounds.Frustum;
+import org.xith3d.loaders.models.impl.obj.OBJLoader;
+import org.xith3d.loaders.models.impl.obj.OBJScene;
+import org.xith3d.loaders.models.impl.dae.collada.ColladaLoader;
+import org.xith3d.loaders.models.impl.dae.DaeLoader;
+import org.xith3d.loaders.models.impl.dae.DaeModel;
+import org.collada._2005._11.colladaschema.COLLADA;
 
 import javax.vecmath.Color3f;
 import javax.vecmath.Vector3f;
 import javax.vecmath.Tuple3f;
-import java.awt.event.MouseWheelListener;
+import javax.vecmath.Point2f;
+import javax.swing.*;
 import java.awt.event.MouseWheelEvent;
-import java.awt.event.MouseMotionListener;
 import java.awt.event.MouseEvent;
 import java.awt.*;
+import java.io.IOException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,9 +33,11 @@ import java.awt.*;
  * To change this template use File | Settings | File Templates.
  */
 public class BasicShapesTest {
-    private static final int MAX_FPS = 50;
+    private static final int MAX_FPS = 100;
 
     private Xith3DEnvironment environment;
+    private Window window;
+    private TransformGroup tgReference;
 
     private volatile boolean rendering = true;
 
@@ -35,47 +45,26 @@ public class BasicShapesTest {
     private Color3f black = new Color3f(0, 0, 0);
     private Color3f blue = new Color3f(0, 0, 1);
     private Color3f red = new Color3f(1, 0, 0);
+    private Color3f green = new Color3f(0,1,0);
+
 
     private Color3f specular = new Color3f(0.9f, 0.9f, 0.9f);
     private volatile int wheelRotation;
     private volatile Tuple3f mouseMovement;
+    private volatile boolean isRecenteringMouse;
 
     private static final Vector3f VECTOR_UP_ORIENTATION = new Vector3f(0, 1, 0);
+
+    private static final float MOUSE_SENSITIVITY = 5;
 
     public void showBasicScene() {
 
         environment = createEnvironment();
 
-
         BranchGroup bg = new BranchGroup();
         environment.addBranchGraph(bg);
 
-        Canvas3DWrapper canvas = (Canvas3DWrapper)environment.getCanvas();
-        canvas.addMouseWheelListener(new MouseWheelListener(){
-
-            public void mouseWheelMoved(MouseWheelEvent e) {
-                wheelRotation = e.getWheelRotation();
-                System.out.println("\n----------------\nwheelRotation = " + wheelRotation);
-            }
-        });
-
-        canvas.addMouseMotionListener(new MouseMotionListener(){
-            private Point lastPosition;
-
-            public void mouseDragged(MouseEvent e) {
-                //do nothing
-            }
-
-            //TODO (pablius) implement!
-            public void mouseMoved(MouseEvent e) {
-                Point currentPoint = e.getPoint();
-                if (lastPosition != null){
-                    //Vector3f currentPosition = new Vector3f(currentPoint.get)
-                }else{
-                    lastPosition = currentPoint;
-                }
-            }
-        });
+        addListeners();
 
         defineView();
 
@@ -85,36 +74,45 @@ public class BasicShapesTest {
 
         setDirectionalLight(bg);
 
-        addSphere(bg);
+        TransformGroup firstTG = addSphere(bg);
+        TransformGroup spaceTG = new TransformGroup();
+        Transform3D t3d = new Transform3D();
+        t3d.setTranslation(300, 0, 0);
+        spaceTG.setTransform(t3d);
+
+
+        DaeModel model = null;
+        try {
+            model = DaeLoader.getInstance().loadModel("C:\\Documents and Settings\\Pablo\\IdeaProjects\\java3dengine\\Xith3DTest\\resources\\models\\collada\\shuttle\\shuttle.dae");
+            assert model != null : "Scene should not be null!";
+
+            spaceTG.addChild(model);
+            firstTG.addChild(spaceTG);
+            tgReference = spaceTG;
+        } catch (Exception e) {
+            throw new RuntimeException("Cancelling scene loading", e);
+        }
+
+
+        SceneGraphObject object = model.getNamedObjects().get("spaceship");
+        Group node = (Group)object;
+        
+        System.out.println("object collidable = " + node.isCollidable());
+
+
 
         startRenderLoop();
 
-        new Thread() {
 
-            public void run() {
-                Thread.currentThread().setName("Wheel rotation thread");
+    }
 
-                while(!rendering){
-                    try{
-                        Thread.sleep(300);
-                    } catch (InterruptedException e) {
-                        System.out.println("Interrupted thread '"+Thread.currentThread().getName()+"' ---------- \n" + e);
-                    }
-                }
 
-                while(rendering){
-                    if (wheelRotation != 0){
-                        zoomView(wheelRotation);
-                        wheelRotation = 0;
-                    }
-                    try {
-                        Thread.sleep(50L);
-                    } catch (InterruptedException e) {
-                        System.out.println("Interrupted thread '"+Thread.currentThread().getName()+"' ---------- \n" + e);
-                    }
-                }
-            }
-        }.start();
+    private void addListeners() {
+        Canvas3DWrapper canvas = (Canvas3DWrapper)environment.getCanvas();
+
+        canvas.addMouseWheelListener(new MyMouseWheelListener());
+
+        canvas.addMouseMotionListener(new MyMouseMotionListener());
     }
 
     private void startRenderLoop() {
@@ -125,6 +123,7 @@ public class BasicShapesTest {
         };
         renderLoop.setMaxFPS(MAX_FPS);
         renderLoop.addRenderEngine(environment);
+
 
         renderLoop.begin();
 
@@ -140,7 +139,7 @@ public class BasicShapesTest {
         }
     }
 
-    private void addSphere(BranchGroup bg) {
+    private TransformGroup addSphere(BranchGroup bg) {
         Material material = new Material(blue, black, blue, specular, 1f);
         material.setLightingEnable(true);
 
@@ -153,10 +152,11 @@ public class BasicShapesTest {
         TransformGroup transformGroup = new TransformGroup();
         transformGroup.addChild(sph);
         Transform3D t3d = new Transform3D();
-        t3d.setTranslation(510, 0, 0);
+        t3d.setTranslation(100, 0, 0);
         transformGroup.setTransform(t3d);
 
         bg.addChild(transformGroup);
+        return transformGroup;
     }
 
     private void setDirectionalLight(BranchGroup bg) {
@@ -164,8 +164,8 @@ public class BasicShapesTest {
         Light lightSource = new DirectionalLight(true, white, lightDirection);//new PointLight(white,lightLocation, lightDirection);
         //bg.addChild(lightSource);
 
-        lightDirection = new Vector3f(1, 2, 1);
-        lightSource = new DirectionalLight(true, red, lightDirection);
+        lightDirection = new Vector3f(1, -2, 1);
+        lightSource = new DirectionalLight(true, green, lightDirection);
         bg.addChild(lightSource);
 
     }
@@ -183,7 +183,7 @@ public class BasicShapesTest {
     }
 
     private void defineView() {
-        Vector3f viewLocation = new Vector3f(400, 0, 0);
+        Vector3f viewLocation = new Vector3f(-100, 0, 0);
         Vector3f viewDirection = new Vector3f(viewLocation);
         viewDirection.add(new Vector3f(1,0,0));
         environment.getView().lookAt(
@@ -200,11 +200,27 @@ public class BasicShapesTest {
         Vector3f facingDirection = view.getFacingDirection();
         facingDirection.add(viewLocation);
         viewDirection = viewDirection != null ? viewDirection : facingDirection;
+        Frustum frustum = null;
+
+
 
         view.lookAt(
                 viewLocation,
                 viewDirection,
                 VECTOR_UP_ORIENTATION);
+    }
+
+    private void moveView(int dx, int dy){
+        View view = environment.getView();
+//        Vector3f facingDirection = view.getFacingDirection();
+//        facingDirection.x += dx;
+//        facingDirection.y += dy;
+        int width = environment.getCanvas().getWidth();//todo (pablius) optimize
+        int height = environment.getCanvas().getHeight();
+        float deltaX = (float)dx / (width/2);
+        float deltaY = (float)dy / (height/2);
+        System.out.println("\nMOVING: deltaY = "+deltaY + ", deltaX = "+deltaX);
+        view.setCenterOfView(new Point2f(deltaX, deltaY));
     }
 
     private void zoomView(int units){
@@ -214,8 +230,13 @@ public class BasicShapesTest {
 
         System.out.println("Before moving view: \nviewLocation = " + viewLocation);
 
-        viewDirection.scale(-5 * units);
-        viewLocation.add(viewDirection);
+        int variation = -5 * units;
+        final Vector3f normalizedViewDirection = new Vector3f(viewDirection);
+        normalizedViewDirection.normalize();
+        normalizedViewDirection.scale(variation);
+
+
+        viewLocation.add(normalizedViewDirection);
 
         System.out.println("After moving view: \nviewLocation = " + viewLocation);
         view.setPosition(viewLocation);
@@ -226,19 +247,129 @@ public class BasicShapesTest {
     private Xith3DEnvironment createEnvironment() {
         Xith3DEnvironment environment = new Xith3DEnvironment();
 
-        Canvas3DWrapper canvas = Canvas3DWrapper.createStandalone(CanvasPeer.OpenGLLayer.JOGL,
-                Canvas3DWrapper.Resolution.RES_1024X768,
-                Canvas3DWrapper.ColorDepth.B32,
-                "BasicScene");
+        Canvas3DWrapper canvas = Canvas3DWrapper.createFullscreen(CanvasPeer.OpenGLLayer.JOGL,
+                Canvas3DWrapper.Resolution.DESKTOP,
+                Canvas3DWrapper.ColorDepth.B32);
+
+//        createStandalone(CanvasPeer.OpenGLLayer.JOGL,
+//                Canvas3DWrapper.Resolution.DESKTOP,
+//                Canvas3DWrapper.ColorDepth.B32,
+//                "BasicScene");
 
 
         environment.addCanvas(canvas);
-        canvas.setRenderOption(Option.USE_TEXTURES, true);
+        //canvas.setRenderOption(Option.USE_TEXTURES, true);
+
+        //set window
+        window = canvas.getWindow();
+
+
+        //set invisible cursor
+        Cursor invisibleCursor =
+            Toolkit.getDefaultToolkit().createCustomCursor(
+            Toolkit.getDefaultToolkit().getImage(""),
+            new Point(0,0),
+            "invisible");
+       // window.setCursor(invisibleCursor);//todo uncomment
+
         return environment;
     }
 
     public static void main(String[] args) {
-        new BasicShapesTest().showBasicScene();
+        try {
+            new BasicShapesTest().showBasicScene();
+        } catch (Exception e) {
+            e.printStackTrace();
+            System.exit(-1);
+        }
+    }
+
+
+    private class MyMouseWheelListener implements java.awt.event.MouseWheelListener {
+        private int despCount = 0;
+        public void mouseWheelMoved(MouseWheelEvent e) {
+            wheelRotation = e.getWheelRotation();
+            System.out.println("\n----------------\nwheelRotation = " + wheelRotation);
+            doZoom(wheelRotation);
+
+
+            if (tgReference != null){
+                despCount += wheelRotation * -10; 
+                Transform3D t3d = new Transform3D();
+                t3d.setTranslation(despCount, despCount /2 , despCount * 3.2f);
+                tgReference.setTransform(t3d);
+            }
+
+        }
+
+        private void doZoom(int wheelRotation){
+            if (wheelRotation != 0){
+                //limit translation, it may move too fast - should be limited in the size of the translation vector
+                wheelRotation = Math.abs(wheelRotation)< 5 ? wheelRotation : ((int)Math.signum(wheelRotation)) * 5;
+                zoomView(wheelRotation);
+            }
+        }
+
+    }
+
+    private class MyMouseMotionListener implements java.awt.event.MouseMotionListener {
+        private Point lastLocation;
+        private Point centerLocation;
+        private Robot robot;
+
+
+        public MyMouseMotionListener() {
+            lastLocation = new Point();
+            centerLocation = new Point();
+            try {
+                robot = new Robot();
+            } catch (AWTException e) {
+                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+            }
+        }
+
+        public void mouseDragged(MouseEvent e) {
+            //do nothing
+        }
+
+        private Point getCenterLocation(){
+            Point result = new Point();
+            result.x = window.getWidth() / 2;
+            result.y = window.getHeight() / 2;
+            return result;
+        }
+        //TODO (pablius) implement!
+        public synchronized void mouseMoved(MouseEvent e) {
+            centerLocation = getCenterLocation();//TODO (pablius) remove from here if unnecessary
+            // this event is from re-centering the mouse - ignore it
+            if (isRecenteringMouse ||
+                    (centerLocation.x == e.getX() && centerLocation.y == e.getY()))
+            {
+                System.out.println("Recentering event processed.");
+                isRecenteringMouse = false;
+            }
+            else {
+                int dx = e.getX() - lastLocation.x;
+                int dy = e.getY() - lastLocation.y;
+                System.out.println("Mouse moved: dy = " + dy + ", dx = " + dx);
+
+                //moveView(dx, dy);   //todo uncomment
+                recenterMouse();  //todo uncomment
+            }
+
+            lastLocation.x = e.getX();
+            lastLocation.y = e.getY();
+
+        }
+
+        private void recenterMouse() {
+            if (robot != null && window.isShowing()) {
+                centerLocation = getCenterLocation();//TODO (pablius) remove from here if unnecessary
+                SwingUtilities.convertPointToScreen(centerLocation, window);
+                isRecenteringMouse = true;
+                robot.mouseMove(centerLocation.x, centerLocation.y);
+            }
+        }
     }
 
 
